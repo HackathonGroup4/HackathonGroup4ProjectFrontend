6e7c06e7269de2838c28906099f4d595f5e42f6d
BinithaShetty <binitharshetty@gmail.com>
2021-07-23 00:02:17 +0800

    removing proxy

--- frontend/src/SearchBox.js
+++ frontend/src/SearchBox.js
@@ -15,7 +15,7 @@ class SearchBox extends React.Component {
   }
 
   handleSubmit() {
-    var url = '/hackathon/ask?query=' + this.state.value
+    var url = 'https://lfrknty04j.execute-api.us-east-1.amazonaws.com/hackathon/ask?query=' + this.state.value
     axios.get(url).then(response => {this.props.onOutputReceived(response.data.command);
     console.log("response",response.data.command)}).catch(error => {
                                                          console.log(error.response.data.error)
