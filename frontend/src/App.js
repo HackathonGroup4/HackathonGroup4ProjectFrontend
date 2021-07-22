fd08174b94ba2fc2fdd7ea2cc9f2fb5d3f5a1386
Zhao Pengfei <zhao.pengfei@u.nus.edu>
2021-07-22 10:56:15 +0800

    push in notification bell icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SearchBox from './SearchBox';
+import NotificationIcon from './notification-icon.png';
 
 function App() {
   return (
     <div className="App">
       <SearchBox />
+      <img src={NotificationIcon} />
       
     </div>
   );
