1793df1c08510883534000b22787004a68d04511
Zhao Pengfei <zhao.pengfei@u.nus.edu>
2021-07-22 14:29:39 +0800

    add in DA icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import SearchBox from './SearchBox';
 import NotificationIcon from './notification-icon.png';
 import HamburgerIcon from './Hamburger-Icon.png';
+import DAIcon from './DA-icon.png';
 import './main.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   <div className="TApp">
     <div className="App" >
           <div className = "Search-area">
+                <img src={DAIcon} className="da-icon" alt="DA icon"/>
                 <SearchBox/>
                 <img src={NotificationIcon} className="notification-icon" alt="notification icon"/>
                 <img src={HamburgerIcon} className="hamburger-icon" alt="menu icon"/>
